ba67cb049d5dd2fb206ccda4f397f1fd6f96c62d
stalker1hunt <forwow30@gmail.com>
2020-01-01 17:12:29 +0200

    Калибрую физику

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -38,40 +38,52 @@ namespace MountainSlide.Player
 
             if (playerRigidbody != null)
             {
-                ApplyInput();
-
                 curentSpeed = playerRigidbody.velocity.y;
 
-                if (MaxSpeed != 0 && CurentSpeed < MaxSpeed)
+                if (Joystick.Horizontal >= 0.2f || (Joystick.Horizontal <= -0.2f))
                 {
-                    curentSpeed = MaxSpeed;
-                    playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, MaxSpeed, playerRigidbody.velocity.z);
+                    playerRigidbody.AddForce(new Vector3(-Joystick.Horizontal, 0, 0), ForceMode.VelocityChange);
                 }
+
+                ApplyInput();
+
+
+                //Mass();
+
+                //if (MaxSpeed != 0 && CurentSpeed < MaxSpeed)
+                //{
+                //    curentSpeed = MaxSpeed;
+                //    //playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, MaxSpeed, playerRigidbody.velocity.z);
+                //}
             }
             else
             {
                 Debug.LogWarning("Rigid body not attached to player " + gameObject.name);
             }
         }
+        [SerializeField]
+        private float myDrag;
+        Vector3 gForceVector = new Vector3(0, 9.81f, 0);
+
+        private void Mass()
+        {
+            Vector3 newVelocity = playerRigidbody.velocity + gForceVector * playerRigidbody.mass * Time.deltaTime;
+            newVelocity = newVelocity * Mathf.Clamp01(1f - myDrag * Time.deltaTime);
+            playerRigidbody.velocity = -newVelocity;
+        }
 
         private void ApplyInput()
         {
             float deltaRotation = Joystick.Horizontal * Time.deltaTime * 4;
 
-            float rotationX = transform.eulerAngles.x;
             float rotationZ = transform.eulerAngles.z * deltaRotation;
             float rotationY = transform.eulerAngles.y * deltaRotation;
 
             rotationY = Mathf.Clamp(rotationY, -45, 45);
             rotationZ = Mathf.Clamp(rotationZ, -45, 45);
 
-            var newRotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
+            var newRotation = Quaternion.Euler(transform.eulerAngles.x, rotationY, rotationZ);
             transform.rotation = newRotation;
-
-            if (Joystick.Horizontal >= 0.2f || (Joystick.Horizontal <= -0.2f))
-            {
-                playerRigidbody.AddForce(new Vector3(-Joystick.Horizontal, 0, 0), ForceMode.VelocityChange);
-            }
         }
 
         public void ApplyBoost(TypeBoost typeBoost)
